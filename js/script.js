bb90e07bce2ac50915023bc113214d8da1e59996
RomanKostyrin <enemy-iubip@mail.ru>
2019-10-20 14:49:00 +0300

    add slider js code

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ var close = document.querySelector(".popup__btn");
 var form = popup.querySelector(".write-us__form");
 var login = popup.querySelector("[name=login]");
 var email = popup.querySelector("[name=email]");
+var radio1 = document.getElementById("radio-1");
+var radio2 = document.getElementById("radio-2");
+var radio3 = document.getElementById("radio-3");
+var slider1 = document.querySelector(".slider-1");
+var slider2 = document.querySelector(".slider-2");
+var slider3 = document.querySelector(".slider-3");
 
 link.addEventListener("click", function(event) {
 event.preventDefault();
@@ -29,5 +35,30 @@ if (event.keyCode === 27) {
 if (popup.classList.contains("popup--show")) {
 popup.classList.remove("popup--show");
 }
+}
+});
+
+radio1.addEventListener("click", function(event) {
+if (!slider1.classList.contains("slider--show")) {
+slider1.classList.add("slider--show");
+slider2.classList.remove("slider--show");
+slider3.classList.remove("slider--show");
+}
+});
+
+radio2.addEventListener("click", function(event) {
+if (!slider2.classList.contains("slider--show")) {
+slider1.classList.remove("slider--show");
+slider2.classList.add("slider--show");
+slider3.classList.remove("slider--show");
+radio2.checked;
+}
+});
+
+radio3.addEventListener("click", function(event) {
+if (!slider3.classList.contains("slider--show")) {
+slider1.classList.remove("slider--show");
+slider2.classList.remove("slider--show");
+slider3.classList.add("slider--show");
 }
 });
